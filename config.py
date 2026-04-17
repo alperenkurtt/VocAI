be297d7dbaf235d2e490f3a642944733d9a4666f
Alperen Kurt <alpereenkurtt@gmail.com>
2026-04-17 20:50:19 +0300

    update config and add dataabse template

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DB_API_ENDPOINT")
+ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
